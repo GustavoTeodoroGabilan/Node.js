18a5955b4143cd54c4eb5b45ff2a4788c23b5b10
Gustavo Teodoro Gabilan <115747012+GustavoTeodoroGabilan@users.noreply.github.com>
2023-08-09 22:16:51 -0300

    Add files via upload

---
+++ Projetos/media.js
@@ -0,0 +1,30 @@
+/**
+ * App para calcular a media de 2 notas
+ */
+
+//variáveis
+let nome, disciplina
+let nota1, nota2, media
+
+console.clear                                                                
+console.log("                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                               .--.      .     .        ")       
+console.log("|   )     |    _|_   o ")         
+console.log("|--:  .-. | .-. |    .  .--.--.") 
+console.log("|   )(   )|(.-' |    |  |  |  | ")
+console.log("'--'  `-' `-`--'`-'-' `-'  '  `-")
+
+//entrada 
+nome = "Gustavo Teodoro"
+disciplina = "Ciencia"
+nota1 = 8
+nota2 = 7
+
+//processamento
+media = (nota1 + nota2) / 2
+
+//saída
+console.log(`Nome: ${nome}`)
+console.log(`Disciplina: ${disciplina}`)
+console.log(`Nota1: ${nota1}`)
+console.log(`Nota2: ${nota2}`)
+console.log(`Nota final: ${media}`)

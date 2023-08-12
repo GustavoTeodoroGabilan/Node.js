07ed92815fea0bb6612e3bae20a6c376d3f494c5
Gustavo Teodoro Gabilan <115747012+GustavoTeodoroGabilan@users.noreply.github.com>
2023-08-11 22:03:36 -0300

    Update media.js

--- Projetos/media.js
+++ Projetos/media.js
@@ -2,29 +2,42 @@
  * App para calcular a media de 2 notas
  */
 
+//importação do pacote readline-sync
+const read = require('readline-sync')
+
 //variáveis
 let nome, disciplina
 let nota1, nota2, media
+const reprovado = false, aprovado=false
 
-console.clear                                                                
-console.log("                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                               .--.      .     .        ")       
+
+console.clear()                                                           
+console.log(".--.      .     .        ")       
 console.log("|   )     |    _|_   o ")         
 console.log("|--:  .-. | .-. |    .  .--.--.") 
 console.log("|   )(   )|(.-' |    |  |  |  | ")
 console.log("'--'  `-' `-`--'`-'-' `-'  '  `-")
 
 //entrada 
-nome = "Gustavo Teodoro"
-disciplina = "Ciencia"
-nota1 = 8
-nota2 = 7
+nome = read.question("Digite seu nome: ")
+disciplina = read.question("Digite a disciplina: ")
+nota1 = Number(read.question("Digite a nota 1: "))
+nota2 = Number(read.question("Digite a nota 2: "))
 
 //processamento
 media = (nota1 + nota2) / 2
 
+if (media>=6)
+    aprovado=true
+    else
+    reprovado=true
+
 //saída
+console.clear()
+console.log("------------Ficha do Aluno---------------")
 console.log(`Nome: ${nome}`)
 console.log(`Disciplina: ${disciplina}`)
 console.log(`Nota1: ${nota1}`)
 console.log(`Nota2: ${nota2}`)
 console.log(`Nota final: ${media}`)
+console.log("-------------------------------------------")

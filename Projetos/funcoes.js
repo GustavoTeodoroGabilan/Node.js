ee02ff2bbd991469040b5cad367137ded156ff90
Gustavo Teodoro Gabilan <115747012+GustavoTeodoroGabilan@users.noreply.github.com>
2023-09-25 21:41:41 -0300

    Add files via upload

---
+++ Projetos/funcoes.js
@@ -0,0 +1,12 @@
+/**
+ * Estudo das funções
+*/
+
+//função simples
+
+function hello(){
+    console.log("hello function")
+}
+
+//para executar a função basta escrever o nome da função junto com parenteses
+hello()

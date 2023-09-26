a0de1bf83bc064c03a69b6d8f223b12086be92bd
Gustavo Teodoro Gabilan <115747012+GustavoTeodoroGabilan@users.noreply.github.com>
2023-09-25 21:53:32 -0300

    Update funcoes.js

--- Projetos/Fundamentos/funcoes.js
+++ Projetos/Fundamentos/funcoes.js
@@ -10,3 +10,20 @@ function hello(){
 
 //para executar a função basta escrever o nome da função junto com parenteses
 hello()
+
+//funções atribuidas
+//função anonima
+const hello2 = function(){
+    console.log("hello function atribuida")
+}
+
+hello2()
+
+//função atribuida simplificada (arrow function)
+const hello3 = () => {
+    console.log("Hello arrow function")
+}
+
+hello3()
+
+console.log(typeof(hello3))
